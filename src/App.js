950545c1c204f3d3e9db3fa8ad89634c89c433bf
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-11 22:51:46 -0500

    app ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+  	super();
+  	this.state={
+  		text:""
+  	};
+  	this.change = this.change.bind(this);
+  }	  
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.text} onChange={this.change}></textarea>
+        <div className="counter">{this.state.text.length}</div>
       </div>
     );
+  };
+
+  change(event){
+  	this.setState({
+  		text: event.target.value
+  	});
   }
 }
 
